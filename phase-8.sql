02eaa0e6caea16d6636d3b6f09d5be365522e57d
HeliaHashemipour <heliahashemipour2@gmail.com>
2023-01-23 17:44:12 +0330

    ADD phase-8

---
+++ phase-8.sql
@@ -0,0 +1,11 @@
+CREATE INDEX enddate_s
+ON Suppliers (enddate);
+
+CREATE INDEX stock_s
+ON sparepart (stock);
+
+CREATE INDEX phonenumber_c
+ON costumers (phonenumber);
+
+CREATE INDEX brandname
+ON brands (name);
