e0cd45c309e9d68a478ae96f43b409f94a6f3ffc
HeliaHashemipour <heliahashemipour2@gmail.com>
2023-01-23 17:43:56 +0330

    ADD phase-7

---
+++ phase-7.sql
@@ -0,0 +1,18 @@
+CREATE ASSERTION stock_assert
+CHECK (
+    SELECT count(*)
+    FROM SparePart
+    WHERE
+          stock=0
+          )=0
+        );
+
+
+CREATE ASSERTION vehicle_order_assert
+CHECK (
+    SELECT count(*)
+    FROM vehicle_orders
+    WHERE counts>=2
+        )=0
+    );
+
